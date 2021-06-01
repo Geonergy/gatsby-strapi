b0fc7899efc0ec35ff3a826d91d0ec88e3fc21c3
philo763 <philo763@users.noreply.github.com>
2021-06-01 15:42:35 +0000

    deploy: a5df634c6c0918d8c390a07f7a8ffe07b036ad05

---
+++ component---src-pages-404-js-acc93a98d0b721284de3.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_strapi_starter=self.webpackChunkgatsby_strapi_starter||[]).push([[883],{9616:function(t,e,a){"use strict";a.r(e);var u=a(5007),l=a(6356),n=a(3751);e.default=function(){return u.default.createElement(l.Z,null,u.default.createElement(n.Z,{title:"404: Not found"}),u.default.createElement("h1",null,"NOT FOUND"),u.default.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-acc93a98d0b721284de3.js.map
