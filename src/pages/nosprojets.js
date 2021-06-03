7805634f5a02186cdb4bf450345e44c7cc9e562e
philo763 <philomene@vergnol.fr>
2021-06-03 19:02:41 +0200

    J'ai ajouté les carrés de services dans la page des projets. J'ai ajouté des commentaires dans la page d'accueil. J'ai modifié le package-lock car sinon je ne pouvais pas gatsby develop. Il faut que tu vois si tu peux le faire avec ma version du package.

--- src/pages/nosprojets.js
+++ src/pages/nosprojets.js
@@ -1,8 +1,33 @@
 import React from "react"
 import PageTemplate from "../components/pageTemplate"
+import { Container, Row, Col } from "reactstrap"
+import { FaGithub, FaBolt, FaHome, FaWrench } from "react-icons/fa"
+import Link from "../components/link"
+import Box from "../components/box"
+
+
+// DEFINITION DU COMPONENT SERVICE
+let Service = ({ title, Icon = FaHome }) => (
+  <Col xs={12} md={4} className="mb-3">
+    <Link to="/">
+      <Box>
+        <Icon size={30} />
+        <h4 className="mt-3">{title}</h4>
+      </Box>
+    </Link>
+  </Col>
+)
 
 let Projets = () => (
   <PageTemplate title="Nos projets">
+    <Container className="py-5">
+      <h2 className="text-center mb-4">Services</h2>
+      <Row>
+        <Service title="Audits énergétiques" />
+        <Service title="Dimensionnement de géothermie" Icon={FaBolt} />
+        <Service title="Suivi des travaux de A à Z" Icon={FaWrench} />
+      </Row>
+    </Container>
     <p>
       Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo erat
       eu viverra mollis. Quisque tellus ante, commodo nec tellus sit amet,
